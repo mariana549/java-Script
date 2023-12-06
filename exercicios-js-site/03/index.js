38e123755ea32776a6bd14b25d501f233494bf9c
mariana549 <marievaristo889@gmail.com>
2023-12-06 18:46:55 -0300

    criando os modelos para os execicios

--- exercicios-js-site/03/index.js
+++ exercicios-js-site/03/index.js
@@ -1,8 +1,7 @@
 // Faça um programa que verifique e mostre os números entre 1.000 e 2.000 (inclusive) que, quando divididos por 11 produzam resto igual a 2.
 
 for (var i = 1000; i <= 2000; i++) {
-   let resultado = i * 11
-   if (resultado % 2 == 0){
-      console.log(resultado)
+   if (i % 11 == 2){
+      console.log("esse é o numero de 1000 a 2000 que dividio por 11 dão resto 2 = " + i)
    }
 }
