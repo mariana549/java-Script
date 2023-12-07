95f02b7db54bdff36953836759cfc4a87a067ac1
mariana549 <marievaristo889@gmail.com>
2023-12-07 15:22:56 -0300

    🍻 atualização

--- exercicios-js-site/04/index.js
+++ exercicios-js-site/04/index.js
@@ -1,2 +1,12 @@
 // Faça um programa que leia um valor n, inteiro e positivo, calcule e mostre a seguinte soma: S = 1 + 1/2 + 1/3 + 1/4 + … + 1/n.
 
+function soma(numero){
+   let somar = numero
+   for(let i = 1; i <= 10; i++){
+      somar += numero / i
+      console.log(somar)
+   }
+
+}
+
+soma(1)
